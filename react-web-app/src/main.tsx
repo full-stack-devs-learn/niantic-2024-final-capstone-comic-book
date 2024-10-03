332a4a651354e8df5b9a4ece75ccf7c7f491a733
Chin <152952438+chinsomnia@users.noreply.github.com>
2024-10-03 15:17:44 -0700

    Updated app name to Comic Circle

--- react-web-app/src/main.tsx
+++ react-web-app/src/main.tsx
@@ -6,9 +6,12 @@ import 'bootstrap/dist/js/bootstrap.min.js'
 import 'bootstrap/dist/css/bootstrap.min.css'
 import { Provider } from 'react-redux'
 import store from './store/store.ts'
+import { StrictMode } from 'react'
 
 createRoot(document.getElementById('root')!).render(
-  <Provider store={store}>
-    <App />
-  </Provider>,
+  <StrictMode>
+    <Provider store={store}>
+      <App />
+    </Provider>,
+  </StrictMode>
 )
