9c159eec2573affb76395f7d96028b3ba3799a4d
Chin <152952438+chinsomnia@users.noreply.github.com>
2024-10-03 19:49:26 -0700

    Created ComicBook model

---
+++ react-web-app/src/models/ComicBook.tsx
@@ -0,0 +1,11 @@
+export class ComicBook
+{
+    comicBookId!: number;
+    marvelId?: number;
+    title!: string;
+    description?: string;
+    imageUrl?: string;
+    detailsUrl?: string;
+    bookCondition?: string;
+    year?: Date;
+}
