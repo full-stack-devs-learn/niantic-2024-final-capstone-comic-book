e3f84ff4e519aa28f808036f5f06a9792e884c91
Chin <152952438+chinsomnia@users.noreply.github.com>
2024-10-04 22:54:28 -0700

    Created Comics component

---
+++ react-web-app/src/components/comics/Comics.tsx
@@ -0,0 +1,29 @@
+export default function Comics({ data }: any)
+{
+    return (
+        <div className="comics">
+            {data.map((comic: any) => {
+                const detailsUrl = comic.urls.find(
+                    (element: any) => element["type"] === "detail"
+                ).url
+
+                return (
+                    <a
+                        key={comic.id}
+                        className="comicCard"
+                        style={{
+                            background: `url(${comic.thumbnail.path}.${comic.thumbnail.extension}) no-repeat center`,
+                            backgroundSize: "cover",
+                        }}
+                        href={detailsUrl}
+                        target="_blank"
+                        rel="noreferrer"
+                    >
+                        <div className="caption">{comic.title}</div>
+                        <div className="caption bottom">View Comic Details</div>
+                    </a>
+                )
+            })}
+        </div>
+    )
+}
