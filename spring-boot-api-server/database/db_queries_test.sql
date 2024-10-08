8ceb20923451023312aeaea1fc1c1c5e045cf04e
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-10-07 22:38:22 -0700

    add getAllTradeComics GetMapping

--- spring-boot-api-server/database/db_queries_test.sql
+++ spring-boot-api-server/database/db_queries_test.sql
@@ -13,3 +13,17 @@ FROM user_collection AS c
 INNER JOIN comic_book AS b
 ON c.comic_book_id = b.comic_book_id
 WHERE user_id = 1;
+
+SELECT
+	  b.comic_book_id
+	, marvel_id
+	, title
+	, description
+	, image_url
+	, details_url
+	, book_condition
+	, published_year
+	, issue_number
+FROM user_trade_collection AS t
+INNER JOIN comic_book AS b
+ON t.comic_book_id = b.comic_book_id;
