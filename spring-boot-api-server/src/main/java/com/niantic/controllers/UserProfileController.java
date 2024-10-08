2db484bf459d10e5ff4031c0e0d7e1b381aab893
Dureti Shemsi <hamzadure@gmail.com>
2024-10-08 12:10:32 -0700

    updated controller and add @preauthorize

--- spring-boot-api-server/src/main/java/com/niantic/controllers/UserProfileController.java
+++ spring-boot-api-server/src/main/java/com/niantic/controllers/UserProfileController.java
@@ -1,9 +1,11 @@
 package com.niantic.controllers;
 
+import com.niantic.data.UserDao;
 import com.niantic.data.UserProfileDao;
 import com.niantic.models.UserProfile;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
+import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.*;
 
 import java.security.Principal;
@@ -15,15 +17,19 @@ import java.util.List;
 public class UserProfileController {
 
     private final UserProfileDao userProfileDao;
+    private final UserDao userDao;
 
     @Autowired
-    public UserProfileController(UserProfileDao userProfileDao) {
+    public UserProfileController(UserProfileDao userProfileDao, UserDao userDao) {
         this.userProfileDao = userProfileDao;
+        this.userDao = userDao;
     }
-
+    
     @GetMapping("/my-profile")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<?> getMyProfile(Principal principal) {
-        UserProfile userProfile = userProfileDao.getUserProfileByEmail(principal.getName());
+        int userId = userDao.getIdByUsername(principal.getName());
+        UserProfile userProfile = userProfileDao.getUserProfileByUserId(userId);
         if (userProfile != null) {
             return ResponseEntity.ok(userProfile);
         }
@@ -31,46 +37,56 @@ public class UserProfileController {
     }
 
     @GetMapping
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<List<UserProfile>> getAllUserProfiles() {
         List<UserProfile> profiles = userProfileDao.getAllUserProfiles();
         return ResponseEntity.ok(profiles);
     }
 
     @GetMapping("/{userId}")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<UserProfile> getUserProfileById(@PathVariable int userId) {
         UserProfile profile = userProfileDao.getUserProfileByUserId(userId);
         if (profile != null) {
             return ResponseEntity.ok(profile);
-        } else {
-            return ResponseEntity.notFound().build();
         }
+        return ResponseEntity.notFound().build();
     }
 
     @PostMapping
-    public ResponseEntity<?> createUserProfile(@RequestBody UserProfile userProfile) {
-        userProfileDao.addUserProfile(userProfile);
-        return ResponseEntity.ok("User profile created successfully.");
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<?> createUserProfile(Principal principal, @RequestBody UserProfile userProfile) {
+        int userId = userDao.getIdByUsername(principal.getName());
+
+        if (userProfileDao.getUserProfileByUserId(userId) == null) {
+            userProfile.setUserId(userId);
+            userProfileDao.addUserProfile(userProfile);
+            return ResponseEntity.ok("User profile created successfully.");
+        }
+        return ResponseEntity.badRequest().body("Profile for this user already exists.");
     }
 
-    @PutMapping("/{userId}")
-    public ResponseEntity<?> updateUserProfile(@PathVariable int userId, @RequestBody UserProfile userProfile) {
+    @PutMapping("/my-profile")
+    @PreAuthorize("isAuthenticated()")
+    public ResponseEntity<?> updateMyProfile(Principal principal, @RequestBody UserProfile updatedProfile) {
+        int userId = userDao.getIdByUsername(principal.getName());
+
         UserProfile existingProfile = userProfileDao.getUserProfileByUserId(userId);
         if (existingProfile != null) {
-            userProfileDao.updateUserProfile(userId, userProfile);
+            userProfileDao.updateUserProfile(userId, updatedProfile);
             return ResponseEntity.ok("User profile updated successfully.");
-        } else {
-            return ResponseEntity.notFound().build();
         }
+        return ResponseEntity.notFound().build();
     }
 
     @DeleteMapping("/{userId}")
+    @PreAuthorize("isAuthenticated()")
     public ResponseEntity<?> deleteUserProfile(@PathVariable int userId) {
         UserProfile existingProfile = userProfileDao.getUserProfileByUserId(userId);
         if (existingProfile != null) {
             userProfileDao.deleteUserProfile(userId);
             return ResponseEntity.ok("User profile deleted successfully.");
-        } else {
-            return ResponseEntity.notFound().build();
         }
+        return ResponseEntity.notFound().build();
     }
 }
