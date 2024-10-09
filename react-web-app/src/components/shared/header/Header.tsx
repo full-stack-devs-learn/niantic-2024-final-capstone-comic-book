913a897a07903020e01bdb0d16e0d76fffa6561a
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-10-09 01:39:28 -0700

    modify navbar, add trade navlink, create user menu dropdown, add icons

--- react-web-app/src/components/shared/header/Header.tsx
+++ react-web-app/src/components/shared/header/Header.tsx
@@ -1,90 +1,105 @@
-import { Link, NavLink } from "react-router-dom"
-import { useAppDispatch } from "../../../store/hooks";
-import { logout } from "../../../store/features/authentication-slice";
-import { useSelector } from "react-redux";
-import { RootState } from "../../../store/store";
+import { Link, NavLink } from 'react-router-dom'
+import { useAppDispatch } from '../../../store/hooks'
+import { logout } from '../../../store/features/authentication-slice'
+import { useSelector } from 'react-redux'
+import { RootState } from '../../../store/store'
+import { BoxArrowRight, Person, Book, BookFill, ArrowLeftRight } from 'react-bootstrap-icons'
+import logoImage from '/images/logo.png'
 import './Header.css'
 
 export default function Header() {
 
-    const dispatch = useAppDispatch()
-    const { isAuthenticated, user } = useSelector((state: RootState) => state.authentication)
+  const dispatch = useAppDispatch()
+  const { isAuthenticated, user } = useSelector((state: RootState) => state.authentication)
 
-    function handleLogout() {
-        localStorage.removeItem('user')
-        dispatch(logout())
-    }
+  function handleLogout() {
+    localStorage.removeItem('user')
+    dispatch(logout())
+  }
 
-    return (
-        <nav className="navbar navbar-expand-lg bg-dark navbar2" data-bs-theme="dark">
-            <div className="container">
-                <div className="collapse navbar-collapse">
-                    <img className="icon" src="/public/logo.png" />
-                    <Link className="navbar-brand comic-circle" to="/">Comic Circle</Link>  
-                </div>
-                <div className="collapse navbar-collapse" id="navbarColor01">
-                    <ul className="navbar-nav ms-md-auto">
-                        {
-                            !isAuthenticated && <>
-                                <li className="nav-item py-2 py-lg-1 col-12 col-lg-auto">
-                                    <div className="vr d-none d-lg-flex h-100 mx-lg-2 text-white"></div>
-                                    <hr className="d-lg-none my-2 text-white-50" />
-                                </li>
-                                <li className="nav-item">
-                                    <NavLink className="nav-link" to="/explore">Explore</NavLink>
-                                </li>
-                                <li className="nav-item py-2 py-lg-1 col-12 col-lg-auto">
-                                    <div className="vr d-none d-lg-flex h-100 mx-lg-2 text-white"></div>
-                                    <hr className="d-lg-none my-2 text-white-50" />
-                                </li>
-                                <li className="nav-item">
-                                    <NavLink className="nav-link" to="/login">Login</NavLink>
-                                </li>
-                                <li className="nav-item py-2 py-lg-1 col-12 col-lg-auto">
-                                    <div className="vr d-none d-lg-flex h-100 mx-lg-2 text-white"></div>
-                                    <hr className="d-lg-none my-2 text-white-50" />
-                                </li>
-                            </>
-                        }
+  return (
+    <nav className="navbar navbar-expand-lg bg-dark navbar2" data-bs-theme="dark">
+      <div className="container">
+        <Link className="navbar-brand d-flex flex-row gap-3 align-items-center" to="/">
+          <img src={logoImage} className="logo-img" alt="Comic Circle Logo" />
+          <h4 className="mb-0">Comic Circle</h4>
+        </Link>
+        <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbar" aria-controls="navbar" aria-expanded="false" aria-label="Toggle navigation">
+          <span className="navbar-toggler-icon"></span>
+        </button>
+        <div className="collapse navbar-collapse" id="navbar">
+          <ul className="navbar-nav">
+            <li className="nav-item py-2 py-lg-1 col-12 col-lg-auto">
+              <div className="vr d-none d-lg-flex h-100 mx-lg-2 text-white"></div>
+              <hr className="d-lg-none my-2 text-white-50" />
+            </li>
+            <li className="nav-item">
+              <NavLink className="nav-link" to="/explore">Explore Comics</NavLink>
+            </li>
+            <li className="nav-item py-2 py-lg-1 col-12 col-lg-auto">
+              <div className="vr d-none d-lg-flex h-100 mx-lg-2 text-white"></div>
+              <hr className="d-lg-none my-2 text-white-50" />
+            </li>
+            {
+              isAuthenticated &&
+              <>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/trade">Trade Comics</NavLink>
+                </li>
+                <li className="nav-item py-2 py-lg-1 col-12 col-lg-auto">
+                  <div className="vr d-none d-lg-flex h-100 mx-lg-2 text-white"></div>
+                  <hr className="d-lg-none my-2 text-white-50" />
+                </li>
+              </>
+            }
+          </ul>
+          <ul className="navbar-nav ms-md-auto">
+            {
+              !isAuthenticated &&
+              <>
+                <li className="nav-item py-2 py-lg-1 col-12 col-lg-auto">
+                  <div className="vr d-none d-lg-flex h-100 mx-lg-2 text-white"></div>
+                  <hr className="d-lg-none my-2 text-white-50" />
+                </li>
+                <li className="nav-item">
+                  <NavLink className="nav-link" to="/login">Login</NavLink>
+                </li>
+                <li className="nav-item py-2 py-lg-1 col-12 col-lg-auto">
+                  <div className="vr d-none d-lg-flex h-100 mx-lg-2 text-white"></div>
+                  <hr className="d-lg-none my-2 text-white-50" />
+                </li>
+              </>
+            }
 
-                        {
-                            isAuthenticated && <>
-                                <li className="nav-item py-2 py-lg-1 col-12 col-lg-auto">
-                                    <div className="vr d-none d-lg-flex h-100 mx-lg-2 text-white"></div>
-                                    <hr className="d-lg-none my-2 text-white-50" />
-                                </li>
-                                <li className="nav-item">
-                                    <button className="nav-link">Collection</button>
-                                </li>
-                                <li className="nav-item py-2 py-lg-1 col-12 col-lg-auto">
-                                    <div className="vr d-none d-lg-flex h-100 mx-lg-2 text-white"></div>
-                                    <hr className="d-lg-none my-2 text-white-50" />
-                                </li>
-                                <li className="nav-item">
-                                    <button className="nav-link" onClick={handleLogout}>Logout</button>
-                                </li>
-                                <li className="nav-item py-2 py-lg-1 col-12 col-lg-auto">
-                                    <div className="vr d-none d-lg-flex h-100 mx-lg-2 text-white"></div>
-                                    <hr className="d-lg-none my-2 text-white-50" />
-                                </li>
-                                <li className="nav-item">
-                                    <button className="nav-link">Hi {user?.username}!</button>
-                                </li>
-                                <li className="nav-item py-2 py-lg-1 col-12 col-lg-auto">
-                                    <div className="vr d-none d-lg-flex h-100 mx-lg-2 text-white"></div>
-                                    <hr className="d-lg-none my-2 text-white-50" />
-                                </li>
-                            </>
-                        }
-                    </ul>
-                </div>
-            </div>
-        </nav>
-
-
-
-
-
-
-    )
+            {
+              isAuthenticated &&
+              <>
+                <li className="nav-item dropdown">
+                  <a className="nav-link dropdown" href="#" role="button" data-bs-toggle="dropdown" aria-expanded="false">
+                    Welcome, {user?.username}!
+                  </a>
+                  <ul className="dropdown-menu">
+                    <li><Link className="dropdown-item d-flex flex-row align-items-center gap-2" to="/profile">
+                      <Person />Profile
+                    </Link></li>
+                    <li><Link className="dropdown-item d-flex flex-row align-items-center gap-2" to="collection">
+                      <BookFill />Collection
+                    </Link></li>
+                    <li><Link className="dropdown-item d-flex flex-row align-items-center gap-2" to="wishlist">
+                      <Book />Wishlist
+                    </Link></li>
+                    <li><Link className="dropdown-item  d-flex flex-row align-items-center gap-2" to="trades">
+                      <ArrowLeftRight />Trades
+                    </Link></li>
+                    <li><hr className="dropdown-divider" style={{ borderColor: 'rgba(255, 255, 255, 0.5)' }} /></li>
+                    <li><button className="dropdown-item d-flex flex-row align-items-center gap-2" onClick={handleLogout}><BoxArrowRight />Logout</button></li>
+                  </ul>
+                </li>
+              </>
+            }
+          </ul>
+        </div>
+      </div>
+    </nav>
+  )
 }
