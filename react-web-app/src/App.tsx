2e98435cf9fc2faafc64c458efff9850600f117a
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-10-09 11:57:12 -0700

    create Footer component, change styles to move it to the bottom of the page

--- react-web-app/src/App.tsx
+++ react-web-app/src/App.tsx
@@ -1,50 +1,42 @@
-import './App.css'
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom'
 import { useSelector } from 'react-redux'
-import { RootState } from "./store/store";
-import { useAppDispatch } from './store/hooks'
+import { RootState } from './store/store'
 import Home from './components/home/Home'
 import Header from './components/shared/header/Header'
+import Footer from './components/shared/footer/Footer'
 import Login from './components/authentication/login/Login'
 import Register from './components/authentication/register/Register'
 import UserHomePage from './components/user-home-page/UserHomePage'
 import Explore from './components/explore/Explore'
-import Details from './components/details/Details';
-import Collection from './components/collection/Collection';
+import Details from './components/details/Details'
+import Collection from './components/collection/Collection'
+
+import './App.css'
 
 function App() {
 
-  const dispatch = useAppDispatch();
-  const { isAuthenticated, user } = useSelector((state: RootState) => state.authentication);
-  const { comicBookId } = useSelector((state: RootState) => state.comicBookId);
+  const { isAuthenticated } = useSelector((state: RootState) => state.authentication);
 
-  // dispatch();
-  
   return (
     <>
-    <Router>
-      <Header />
+      <Router>
+        <Header />
 
-      <main className="container mt-4">
-      <Routes>
-        <Route path='/login' element={<Login />} />
-        <Route path='/register' element={<Register />} />
-        <Route path='/explore' element={<Explore />} />
+        <main className="container p-4">
+          <Routes>
+            <Route path='/login' element={<Login />} />
+            <Route path='/register' element={<Register />} />
+            <Route path='/explore' element={<Explore />} />
+            {!isAuthenticated && <Route path='/' element={<Home />} />}
+            {isAuthenticated && <Route path='/' element={<UserHomePage />} />}
+            {isAuthenticated && <Route path=':comicBookId' element={<Details />} />}
+            {isAuthenticated && <Route path='/collection' element={<Collection />} />}
 
-        { !isAuthenticated && <Route path='/' element={<Home />} />}
-        { isAuthenticated && <Route path='/' element={<UserHomePage />} />}
-        { isAuthenticated && <Route path=':comicBookId' element={<Details />} />}
-        { isAuthenticated && <Route path='/collection' element={<Collection />} />}
+          </Routes>
+        </main>
 
-      </Routes>
-      </main>
-
-    </Router>
-      <footer>
-        <div className='cc-footer'>
-          &copy; Comic Circle 2024
-        </div>
-      </footer>
+        <Footer />
+      </Router>
     </>
   )
 }
