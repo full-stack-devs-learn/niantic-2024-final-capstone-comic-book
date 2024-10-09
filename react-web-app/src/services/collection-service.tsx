444a87a7044aaa5237fc36d651c9518f803ef849
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-10-09 13:59:48 -0700

    update collection-service, add updateComicBookCondition method

--- react-web-app/src/services/collection-service.tsx
+++ react-web-app/src/services/collection-service.tsx
@@ -1,47 +1,46 @@
-import axios, { AxiosRequestConfig } from "axios";
-import { ComicBook } from "../models/ComicBook";
-import store, { RootState } from "../store/store";
+import axios, { AxiosRequestConfig } from "axios"
+import { ComicBook } from "../models/ComicBook"
+import store, { RootState } from "../store/store"
 
-class CollectionService
-{
-    baseUrl = `${import.meta.env.VITE_API_BASE_URL}/comics/collection`;
+class CollectionService {
+  baseUrl = `${import.meta.env.VITE_API_BASE_URL}/comics/collection`
 
-    createHeaders(): AxiosRequestConfig
-    {
-        const state: RootState = store.getState();
-        const token = state.authentication.token;
+  createHeaders(): AxiosRequestConfig {
+    const state: RootState = store.getState()
+    const token = state.authentication.token
 
-        const config: AxiosRequestConfig<any> = {
-            headers: {
-                'Content-Type': 'application/json'
-            }
-        }
-
-        if(token !== null)
-        {
-            config.headers!['Authorization'] = `Bearer ${token}`;
-        }
-
-        return config;
+    const config: AxiosRequestConfig<any> = {
+      headers: {
+        'Content-Type': 'application/json'
+      }
     }
 
-    async getUserCollection(): Promise<ComicBook[]>
-    {
-        const response = await axios.get<ComicBook[]>(this.baseUrl, this.createHeaders());
-        return response.data;
+    if (token !== null) {
+      config.headers!['Authorization'] = `Bearer ${token}`
     }
 
-    async addComicBookToUserCollection(comicBook: ComicBook): Promise<ComicBook>
-    {
-        const response = await axios.post<ComicBook>(this.baseUrl, comicBook, this.createHeaders());
+    return config
+  }
 
-        return response.data;
-    } 
+  async getUserCollection(): Promise<ComicBook[]> {
+    const response = await axios.get<ComicBook[]>(this.baseUrl, this.createHeaders())
+    return response.data
+  }
 
-    async removeComicBookToUserCollection(comicBookId: number) {
-        return axios.delete<void>(`${this.baseUrl}/${comicBookId}`)
-    }
+  async addComicBookToUserCollection(comicBook: ComicBook): Promise<ComicBook> {
+    const response = await axios.post<ComicBook>(this.baseUrl, comicBook, this.createHeaders())
+    return response.data
+  }
+
+  async updateComicBookCondition(comicBook: ComicBook): Promise<ComicBook> {
+    const response = await axios.put<ComicBook>(`${this.baseUrl}/${comicBook.comicBookId}`, comicBook, this.createHeaders())
+    return response.data
+  }
+
+  async removeComicBookFromUserCollection(comicBookId: number) {
+    return axios.delete<void>(`${this.baseUrl}/${comicBookId}`, this.createHeaders())
+  }
 }
 
-const collectionService = new CollectionService();
-export default collectionService;
+const collectionService = new CollectionService()
+export default collectionService
