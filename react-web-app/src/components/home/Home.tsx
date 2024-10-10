8f35b8562e518e844f425d7a68e6b56bbe5e4553
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-10-10 14:29:28 -0700

    replace button with Link to register on Home Page

--- react-web-app/src/components/home/Home.tsx
+++ react-web-app/src/components/home/Home.tsx
@@ -1,39 +1,35 @@
 import { useState } from "react";
 import demoService from "../../services/demo-service"
 import './Home.css'
+import { Link } from 'react-router-dom';
 
-export default function Home()
-{
-    const [data, setData] = useState<any>('');
+export default function Home() {
+  const [data, setData] = useState<any>('');
 
-    async function simpleClickHandler()
-    {
-        const result = await demoService.getDemo()
-        console.log(result);
-        setData(result)
-        
-    }
+  async function simpleClickHandler() {
+    const result = await demoService.getDemo()
+    console.log(result);
+    setData(result)
+  }
 
-    return (
-        <>
-            <div className="home-container">
-                <div className="img-container">
-                    <h1 className="title">Level Up Your Comic Stash</h1>
-                    <img className="spidey" src="/public/spidey.png"/>
-                </div>
-                {/* <div className="title">
-                </div> */}
-                <div className="subtitle">
-                    <p>The only place where hoarding is a superpower!</p>
-                </div>
-                <div className="create-btn">
-                    <button type="button" className="btn btn-info btn-lg" onClick={simpleClickHandler}>Create new account</button>
-                </div>
-                <div>
-                    {data}
-                </div>
-
-            </div>
-        </>
-    )
+  return (
+    <>
+      <div className="home-container">
+        <div className="img-container">
+          <h1 className="title">Level Up Your Comic Stash</h1>
+          <img className="spidey" src="/spidey.png" />
+        </div>
+        <div className="subtitle">
+          <p>The only place where hoarding is a superpower!</p>
+        </div>
+        <div className="create-btn">
+          <Link to="/register" className="btn btn-info btn-lg">Create New Account</Link>
+          {/* <button type="button" className="btn btn-info btn-lg" onClick={simpleClickHandler}>Create new account</button> */}
+        </div>
+        <div>
+          {data}
+        </div>
+      </div>
+    </>
+  )
 }
