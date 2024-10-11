ae9c99ec38a647cf4b691e3fc792a38ef8dfff7c
Chin <152952438+chinsomnia@users.noreply.github.com>
2024-10-11 00:05:19 -0700

    Removed unnecessary comments

--- react-web-app/src/store/store.ts
+++ react-web-app/src/store/store.ts
@@ -1,12 +1,9 @@
 import { configureStore } from "@reduxjs/toolkit";
 import authenticationReducer from "./features/authentication-slice";
 import collectionReducer from "./features/collection-slice";
-// import comicBookDataReducer from "./features/comic-details-slice";
 import comicsSearchResultsReducer from "./features/comics-search-result-slice";
 import characterSearchResultsReducer from "./features/character-search-result-slice";
 import wishlistReducer from './features/wishlist-slice';
-// import searchTermReducer from "./features/search-term-slice";
-// import comicBookIdReducer from "./features/comic-details-slice";
 import userProfileReducer from './features/user-profile-slice';
 
 const store = configureStore({
@@ -14,11 +11,8 @@ const store = configureStore({
         authentication: authenticationReducer,
         collection: collectionReducer,
         wishlist: wishlistReducer,
-        // comicBookDetail: comicBookDataReducer,
         comicsSearchResults: comicsSearchResultsReducer,
         characterSearchResults: characterSearchResultsReducer, 
-        // searchTermReducer
-        // comicBookId: comicBookIdReducer,
         userProfile: userProfileReducer,
     }
 })
