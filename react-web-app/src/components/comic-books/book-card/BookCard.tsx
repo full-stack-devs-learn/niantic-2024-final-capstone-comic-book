822eed502fb797d75b7979414f921f40a45044a7
Elena Bychenkova <elena.bychenkova@gmail.com>
2024-10-11 02:37:18 -0700

    implement editing book condition for books in trade collection

--- react-web-app/src/components/comic-books/book-card/BookCard.tsx
+++ react-web-app/src/components/comic-books/book-card/BookCard.tsx
@@ -1,6 +1,12 @@
+import { useState } from 'react';
 import { ComicBook } from '../../../models/ComicBook';
 import { useAppDispatch } from '../../../store/hooks';
-import { addComicBookToCollection, removeComicBookFromUserCollection, removeComicBookFromCollection } from '../../../store/features/collection-slice';
+import {
+  addComicBookToCollection,
+  removeComicBookFromUserCollection,
+  removeComicBookFromCollection,
+  updateComicBookCondition
+} from '../../../store/features/collection-slice';
 import { removeComicBookFromUserWishlist } from '../../../store/features/wishlist-slice';
 import { removeComicBookFromUserTradeCollection, addComicBookToUserTradeCollection } from '../../../store/features/trade-collection-slice';
 import { clear as clearTradeComics } from '../../../store/features/trade-comics-slice'
@@ -15,6 +21,9 @@ interface BookCardProps {
 export default function BookCard({ book, type }: BookCardProps) {
 
   const dispatch = useAppDispatch()
+
+  const [isEditing, setIsEditing] = useState(false);
+  const [condition, setCondition] = useState(book.bookCondition || '');
 
   const handleDeleteBook = async (event: React.MouseEvent<SVGElement, MouseEvent>) => {
     event.stopPropagation();
@@ -54,12 +63,34 @@ export default function BookCard({ book, type }: BookCardProps) {
   const handleUpdateCondition = (event: React.MouseEvent<SVGElement, MouseEvent>) => {
     event.stopPropagation();
     console.log('Edit condition of book:', book.title);
+    setIsEditing(true);
   }
+
+  const handleSaveCondition = async (event: React.MouseEvent<HTMLButtonElement, MouseEvent>) => {
+    event.preventDefault();
+    event.stopPropagation();
+    dispatch(updateComicBookCondition({ ...book, bookCondition: condition }));
+    setIsEditing(false);
+  }
+
 
   return (
     <div className="book-card card d-flex flex-column align-items-center gap-3 justify-content-end">
-      <h5 className="text-center">{book.title}</h5>
+      <h5 className="text-center mb-0">{book.title}</h5>
       <img src={book.imageUrl} alt={`${book.title} cover`} className="card-image" />
+      {type === 'trade-collection' || type === 'trade-comics' ?
+        isEditing ? (
+          <div className="condition-input w-100">
+            <input
+              type="text"
+              value={condition}
+              onChange={(e) => setCondition(e.target.value)}
+            />
+            <button onClick={handleSaveCondition}>Save</button>
+          </div>
+        ) : (
+          <p className="condition mb-0">{condition}</p>
+        ) : null}
       <div className="d-flex flex-row justify-content-between align-items-center h-5 mb-0 w-100">
         {type === 'collection' &&
           <>
